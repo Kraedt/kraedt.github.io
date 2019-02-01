d29da6b2fefef1f7b58044921d65c443e01f0ca0
Chris <cschwartz110@gmail.com>
2019-02-01 18:34:45 -0500

    Redesign finished for home page

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll --watch';
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
@@ -14,7 +14,7 @@ var messages = {
  */
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+    return cp.spawn(jekyll , ['build'], {stdio: 'inherit'})
         .on('close', done);
 });
 
